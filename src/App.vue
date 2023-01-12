200f8ce9228840d3b1373802188152a73885a3cf
roulios44 <jules.croizier@gmail.com>
2023-01-12 16:15:56 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Yo mister white bitch"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//const mysql = require('mysql');
+console.log("hi");
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Hello bitch</h1>
+</template>
+<script>
+</script>
