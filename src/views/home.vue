708ec03c8743aa5d327abcd984e8f95736a7b416
NRIVALANT <nicolas.rivalant@ynov.com>
2023-01-25 11:40:57 +0100

    restore

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,34 +8,35 @@
     </div>
   </nav>
   <form id="app" @submit="checkForm" action="/something" method="post">
-  <p>
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name" v-model="name">
-  </p>
-  <form id="app" @submit="checkForm" action="/something" method="post">
-  <p>
-    <h1>Hello</h1>
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name" v-model="name">
-  </p>
+  
+    <p>
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" v-model="name">
+    </p>
+    <form id="app" @submit="checkForm" action="/something" method="post">
+    <p>
+      <h1>Hello</h1>
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" v-model="name">
+    </p>
 
-  <p>
-    <label for="age">Age</label>
-    <input type="number" name="age" id="age" v-model="age" min="0">
-  </p>
+    <p>
+      <label for="age">Age</label>
+      <input type="number" name="age" id="age" v-model="age" min="0">
+    </p>
 
-  <p>
-    <label for="movie">Favorite Movie</label>
-    <select name="movie" id="movie" v-model="movie">
-      <option>Star Wars</option>
-      <option>Vanilla Sky</option>
-      <option>Atomic Blonde</option>
-    </select>
-  </p>
+    <p>
+      <label for="movie">Favorite Movie</label>
+      <select name="movie" id="movie" v-model="movie">
+        <option>Star Wars</option>
+        <option>Vanilla Sky</option>
+        <option>Atomic Blonde</option>
+      </select>
+    </p>
 
-  <p>
-    <input type="submit" value="Submit">  
-  </p>
+    <p>
+      <input type="submit" value="Submit">  
+    </p>
 
   </form>
 </template>
