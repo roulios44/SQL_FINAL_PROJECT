852e24bdfe14079f9ca6c30bea76b0dea2e54523
roulios44 <jules.croizier@gmail.com>
2023-01-25 10:09:03 +0100

    retrive departemens from DB

---
+++ src/views/getAllDepartement.vue
@@ -0,0 +1,30 @@
+<template>
+    {{
+        allDepartements
+     }}
+</template>
+
+
+<script>
+
+
+export default {
+    data() {
+      return {
+        allDepartements: [],
+      }
+    },
+    methods: {
+      async createDepartement(){
+        const res = await fetch("http://localhost/SQL_FINAL_BACK/getAllDepartement.php")
+        this.allDepartements = await res.json()
+        }
+    },
+    async mounted() {
+        this.createDepartement()
+    }
+}
+</script>
+<style>
+</style>
+
