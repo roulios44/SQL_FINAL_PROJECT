e12a2e7cfd882bfae3f07286622659755c387e80
roulios44 <jules.croizier@gmail.com>
2023-01-30 16:51:24 +0100

    get info for employee

--- src/views/profilEmployee.vue
+++ src/views/profilEmployee.vue
@@ -1,6 +1,7 @@
 <template>
     <navBar />
     {{ this.idEmployee }}
+    {{ this.infoEmployee }}
 </template>
 <script>
 import axios from 'axios'
@@ -9,6 +10,7 @@ export default {
   data() {
     return {
         idEmployee:"",
+        infoEmployee:{},
     }
   },
   components: {
@@ -19,10 +21,18 @@ export default {
         let queryString = window.location.search;
         let urlParams = new URLSearchParams(queryString);
         this.idEmployee = urlParams.get("id")
-    }
+    },
+    async getInfoEmployee(){
+        const post = await axios.post("http://localhost/SQL_FINAL_BACK/infoForProfil.php",JSON.stringify({
+            "id":this.idEmployee,
+        }))
+        const data = await post.data
+        this.infoEmployee = data
+    },
   },
   async mounted(){
       await this.getIdEmployee()
+      await this.getInfoEmployee()
     }
 }
 </script>
