f56b2c42b34c41e7eb2ebaf420847d4f0f5004ae
Lisachard <lisa.achard@ynov.com>
2023-01-30 20:23:17 +0100

    Minor mistakes fixed

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,9 +4,11 @@
 
 <script>
 import navBar from '@/components/navBar.vue'
+import common from '@/components/common.vue'
   export default {
     components:{
-      navBar
+      navBar,
+      common
     },
   };
 </script>
